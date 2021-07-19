db3e85a8f0f0b8cbde0e7fc0d57a332ee6fb317a
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-07-19 10:37:16 -0300

    Teste migration

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ require('./database');
 const app = express();
 const port = process.env.PORT || 3333;
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use(express.json());
 app.use(routes);
